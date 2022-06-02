1dff9bcc5d4c354bbf193ebfb91361bd3dbf0629
Sofibright <coaz@mail.ru>
2022-06-03 02:41:12 +1000

    if else

--- pr.py
+++ pr.py
@@ -1,3 +1,17 @@
 x = 5
 y = 10
 print(y > x * x or y >= 2 * x and x < y)
+# Условия if else
+# A = int(input("Время, которое рекомендовали спать"))
+# B =  int(input("Время, которое является вредным для сна"))
+# H = int(input("Сколько сейчас спите?"))
+# if H>=A and H<B:
+#     print("Это нормально")
+# else:
+#     if H>B:
+#         print("Пересып")
+#     else:
+#         print("Недосып")
+
+n = int(input())
+
