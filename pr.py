6533bfba1e8d8813d6fb714563860162a00f42aa
Sofibright <coaz@mail.ru>
2022-06-20 02:43:08 +1000

    while

--- pr.py
+++ pr.py
@@ -13,15 +13,42 @@ print(y > x * x or y >= 2 * x and x < y)
 #     else:
 #         print("Недосып")
 
-n = int(input())
-if n%4!=0:
-    print("Обычный")
-else:
-    if n%100==0:
-        if n%400==0:
-            print("Високосный")
-        else:
-            print("Обычный")
+# n = int(input())
+# if n%4!=0:
+#     print("Обычный")
+# else:
+#     if n%100==0:
+#         if n%400==0:
+#             print("Високосный")
+#         else:
+#             print("Обычный")
+#
+#     else:
+#          print("Високосный")
 
-    else:
-         print("Високосный")
+# import math
+# a = int(input())
+# b = int(input())
+# c = int(input())
+# p = ((a+b+c)/2)
+# S = math.sqrt(p*(p-a)*(p-b)*(p-c))
+# print(S)
+
+
+# a = int(input())
+# if a>-15 and a<= 12:
+#     print("True")
+# else:
+#     if (a>=14 and a<=17):
+#         print("True")
+#     else:
+#         if a>19:
+#             print("True")
+#         else:
+#             print("False")
+# i = 0
+# while i <= 10:
+#     i = i + 1
+#     if i > 7:
+#         i = i + 2
+#         print(i)
