827be900ea41987999de593a31e1cb7d26ada8d8
Sofibright <coaz@mail.ru>
2023-01-16 04:57:38 +1000

    while new task new 2

--- main.py
+++ main.py
@@ -9,3 +9,14 @@
 # print(((x+h*60+m)%60))
 # z = [int(s) for s in input().split()]
 # print(sum(z))
+# a = [int(i) for i in input().split()]
+# if len(a) ==1:
+#     print(a[0])
+# else:
+#     b = str()
+#     for i in range(0, len(a)):
+#         buffer = a[i - 1] + a[(i + 1) % len(a)]
+#         b += str(buffer) + ' '
+#     print(b)
+
+
